37bddd9a4e58c9345001ab61381746ada0cc01ac
Tom Ridd <twridd@gmail.com>
2019-10-15 12:11:45 +0100

    ready for deployment

--- index.js
+++ index.js
@@ -20,9 +20,18 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-
 require('./routes/authRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+    // serve static assets
+    app.use(express.static('client/build'));
+
+    // serve index.html file
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
